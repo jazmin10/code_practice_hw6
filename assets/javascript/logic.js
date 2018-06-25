15ef1c3522290a5068fb7b2d6b7b666425521b82
jazmin10 <jazminestrada10@gmail.com>
2018-06-25 12:03:55 -0500

    Added event listener and callback function for when a new animal topic is added

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,8 +4,12 @@ $(document).ready(function() {
 var topics = ["cat", "pugs", "puppies", "birds"];
 
 // =========== FUNCTIONS ===========
+
+// Display the list of topics as buttons
 function displayButtons() {
-	
+
+	$("#buttons-section").empty();
+
 	topics.forEach(function(topic) {
 		var topicButton = $("<button>").attr("value", topic);
 		topicButton.addClass("btn btn-info btn-lg");
@@ -15,8 +19,30 @@ function displayButtons() {
 	});
 }
 
+// Add a topic to the topics list
+function addTopic() {
+	event.preventDefault();
+
+	// Stores the user's entry by lowercasing the entry and replacing " " with "+"
+	var userEntry = $("#new-topic").val().trim();
+	var newTopic = userEntry.toLowerCase().replace(/ /g, "+");
+
+	$("#new-topic").val("");
+
+	// Add topic to the topics array 
+	topics.push(newTopic);
+
+	// Display the new list of topics
+	displayButtons();
+
+}
+
 // =========== MAIN PROCESSES ===========
+
+// When page loads, display topics buttons
 displayButtons();
 
+// When the form is filled out, add new topic to the topics list
+$("#add-topic-btn").click(addTopic);
 
 });
